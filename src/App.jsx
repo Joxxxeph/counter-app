71e04d911492043b961fb644f40d82c738a1c94a
jvasquez12 <jvasquez@nerdcom.do>
2024-06-22 22:14:08 -0400

    counter

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Counter from './components/Counter'
+
+function App() {
+
+  return (
+    <main className="font-['Montserrat'] h-max lg:h-screen flex items-center justify-center bg-[#f9efe6] max-w-full">
+      <Counter />
+    </main>
+  )
+}
+
+export default App
